59dfe09101e7c76566f23caa645ec7c6f8645c03
Manos Saratsis <manos.saratsis@gmail.com>
2018-02-07 11:47:30 -0800

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ TESTS_REQUIRE = [str(r.req) for r in
 
 setup(
     name='sscpy',
-    version='0.1.2',
+    version='0.1.3',
     description='SAM Simulation Core (SSC) API for Python',
     author='Station A',
     author_email='StationAOps@nrg.com',
