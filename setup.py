02d9c640bcb80d71074c9deef9ceac7415ff3faf
Denise <denise.rivas@nrg.com>
2018-05-07 11:11:04 -0700

    [Hotfix] Updating per new pip version

--- setup.py
+++ setup.py
@@ -1,13 +1,4 @@
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
-from pip.download import PipSession
-
-
-SESSION = PipSession()
-INSTALL_REQUIRES = [str(r.req) for r in
-                    parse_requirements('./requirements.txt', session=SESSION)]
-TESTS_REQUIRE = [str(r.req) for r in
-                 parse_requirements('./test_requirements.txt', session=SESSION)]
 
 
 setup(
@@ -24,7 +15,5 @@ setup(
     zip_safe=False,
     setup_requires=['cffi>=1.0.0'],
     cffi_modules=['ssc/ffibuilder.py:_FFI'],
-    install_requires=INSTALL_REQUIRES,
-    tests_require=TESTS_REQUIRE,
     license='License :: OSI Approved :: MIT License',
 )
