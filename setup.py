6b06c396bea764e8d17eef9206a3a3d7d50d6cd0
jerluc <me@jerluc.com>
2018-06-18 11:16:22 -0700

    Bump version: 0.1.6 → 0.1.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sscpy',
-    version='0.1.6',
+    version='0.1.7',
     description='SAM Simulation Core (SSC) API for Python',
     author='Station A',
     author_email='StationAOps@nrg.com',
