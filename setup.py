cc60255f95a0ba65c8fb1b9270f397f0fd8eb098
jerluc <me@jerluc.com>
2020-10-16 15:24:26 -0700

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(curdir, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="sscpy",
-    version="0.2.0",
+    version="0.2.1",
     description="Python bindings for SAM Simulation Core (SSC)",
     long_description=long_description,
     long_description_content_type="text/markdown",
